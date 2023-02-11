ea24fe55108c43508fd1c743a4ff33feada21d1e
Natallia <natashaivanyuk@gmail.com>
2023-02-11 20:29:37 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let open = [];
+  let exeptions = [];
+  bracketsConfig.forEach(item => {
+    if (item[0] === item[1]) {
+      exeptions.push(item[0])
+    } else {
+      open.push(item[0]);
+    }
+  })
+  let brackets = Object.fromEntries(bracketsConfig);
+  for (let i = 0; i < str.length; i++) {
+    let lastInStack = stack[stack.length - 1];
+    if (open.includes(str[i])) {
+      stack.push(str[i]);
+    } else if ((exeptions.includes(str[i])) && (lastInStack !== str[i])) {
+      stack.push(str[i]);
+    } else if ((!open.includes(str[i])) && (stack.length === 0)) {
+      return false;
+    } else if ((exeptions.includes(str[i])) && (lastInStack === str[i])) {
+      stack.pop();
+    } else if (str[i] === brackets[lastInStack]) {
+      stack.pop();
+    } 
+  }
+  return stack.length === 0;
 }
